bd199841e1e447682d188ca2f35bcedca2b43199
Lina Del Bondio <linadelbondio@sfs-wifi-dhcp-10-143-177-152.wifi.sfsu.edu>
2018-05-15 11:35:23 -0700

    Updated tab controller

---
+++ DrinkShakerProject/DrinkShakerProject/AllDrinksController.swift
@@ -0,0 +1,32 @@
+//
+//  AllDrinksController.swift
+//  DrinkShakerProject
+//
+//  Created by Lina Del Bondio on 5/15/18.
+//  Copyright © 2018 Shayn. All rights reserved.
+//
+
+import UIKit
+
+
+class allCell: UITableViewCell {
+    
+    
+}
+
+class AllDrinksController: UITableViewController {
+    
+    var drinks: [String] = ["cosmo", "martini", "jack"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return drinks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let drink = drinks[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "drinkCell")!
+        cell.textLabel?.text = drink
+        return cell
+    }
+}
---
+++ DrinkShakerProject/DrinkShakerProject/customCell.swift
@@ -0,0 +1,24 @@
+//
+//  customCell.swift
+//  DrinkShakerProject
+//
+//  Created by Lina Del Bondio on 5/15/18.
+//  Copyright © 2018 Shayn. All rights reserved.
+//
+
+import UIKit
+
+class customCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
