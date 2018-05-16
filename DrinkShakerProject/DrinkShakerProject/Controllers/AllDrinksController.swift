928af470a508179de359a705828d6ec7873a1d16
rushvora <rushvora@gmail.com>
2018-05-15 19:12:31 -0700

    Added hardcoded array

--- DrinkShakerProject/DrinkShakerProject/Controllers/AllDrinksController.swift
+++ DrinkShakerProject/DrinkShakerProject/Controllers/AllDrinksController.swift
@@ -15,37 +15,40 @@ class allCell: UITableViewCell {
 
 class AllDrinksController: UITableViewController {
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 200
     }
+    var drinks: [String] = ["cosmopolitan", "martini", "jack"]
+    var ingredientsList: [String] = [""]
+    var ratingList : [String] = ["4/5", "3/5", "4/5", "2/5"]
+    var photoList : [UIImage] = [#imageLiteral(resourceName: "cosmo")]
     
-/*    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return drinks.count
     }
-    */
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//
-//    }
-//
+    
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //
+    //    }
+    //
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        
-        
-
-//      let cell = tableView.dequeueReusableCell(withIdentifier: "drinkCell", for: indexPath)
+        let drink = drinks[indexPath.row]
+        let ingredients1 = ingredientsList[indexPath.row]
+        let ratings1 = ratingList[indexPath.row]
+        let photo1 = photoList[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "drinkCell") as! customCell
-
-        cell.photo.image = #imageLiteral(resourceName: "cosmo")
-        cell.rating.text = "Rating: 1/5"
-        cell.ingredients.text = "1/2 oz lemon juice \n4oz booze"
+        
+        
+        cell.drinkName.text = drink
+        cell.photo.image = photo1
+        cell.rating.text = ratings1
+        cell.ingredients.text = ingredients1
         //cell.textLabel?.text = drink
-
-        performSegue(withIdentifier: "drinkInfoSegue", sender: self)
-
-
+       // performSegue(withIdentifier: "drinkInfoSegue", sender: self)
+        
         return cell
     }
 }
